deb695838ab620360d8a6a5589c4ed42f503a742
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-09-07 18:27:04 -0300

    Criando e estilizando conteúdo da Home

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import './styles.jsx'
+import { Container, ContainerHello, Title, Subtitle, Description, Button, ContainerImages } from './styles.jsx'
 
-import Header from '../../components/Header/header'
+import Header from '../../components/Header/header';
+import Footer from '../../components/Footer/footer';
+
+import Panel from '../../Assets/panel.png'
 
 export default function Home() {
     return (
         <div>
             <Header />
+
+            <Container>
+                <ContainerHello>
+                    <Title>{`FREE`}</Title>
+                    <Subtitle>{`YOURSELF`} </Subtitle>
+                    <Description>{`Uma descrição bem topada sobre mim ou sobre meu trabalho onde
+                     as pessoas poderão se engajar em ver meu conteúdo nas
+                      redes sociais e até mesmo compartilhar com os amigos.`}</Description>
+                    <Button>Começar tour</Button>
+                </ContainerHello>
+
+                <ContainerImages>
+                    <img src={Panel}/>
+                </ContainerImages>
+            </Container>
+
+            <Footer />
         </div>
     )
 }
---
+++ src/pages/Home/styles.jsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    margin-top: 1%;
+    display: flex;
+    margin-left: 6%;
+    margin-right: 6%;
+    width: 90%;
+    height: 860px;
+`;
+
+export const ContainerHello = styled.div`
+    margin-top: 8%;
+    width: 50%;
+`;
+
+export const Title = styled.h1`
+    color: #000000;
+    margin-bottom: 0;
+    font-size: 64pt;
+    font-weight: bold;
+`;
+
+export const Subtitle = styled.p`
+    color: #000000;
+    margin-top: 0;
+    margin-bottom: 0;
+    font-size: 48pt;
+    font-weight: bold;
+`;
+
+export const Description = styled.h1`
+    width: 58%;
+    margin-top: 2%;
+    font-size: 16pt;
+    font-weight: 400;
+    color: #6D747A
+`;
+
+export const Button = styled.div`
+    border: 2px solid #6D747A;
+    border-radius: 4px;
+    color: #6D747A;
+    width: 120px;
+    height: 30px;
+    text-align: center;
+    font-size: 14pt;
+    font-weight: 500;
+    margin-top: 2%;
+    padding-top: 1%;
+    padding-left: 2%;
+    padding-right: 2%;
+`;
+
+export const ContainerImages = styled.div`
+    width: 50%;
+    margin-top: 4%;
+    margin-right: 4%;
+    padding-top: 4%;
+    padding-bottom: 4%;
+    height: 60%;
+`;
+
