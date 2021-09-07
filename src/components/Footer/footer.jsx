a7a3c0189dbfcb6bc33696549fcb125ca7ba0d17
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-09-07 18:26:07 -0300

    Criando e estilizando o componente Footer

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Copyright } from './styles.jsx'
+
+export default function Footer() {
+    return (
+        <Copyright> DIREITOS AUTORAIS - 2021</Copyright>
+    )
+}
---
+++ src/components/Footer/styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const Copyright = styled.h1`
+    width: 100%;
+    text-align: center;
+    font-size: 14pt;
+    color: #6D747A;
+    margin-bottom: 1%;
+`;
