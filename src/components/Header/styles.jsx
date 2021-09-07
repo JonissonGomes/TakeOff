2c5f0af6f25c40bf9103985f3791c5b13f7f9ad9
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-09-07 17:10:58 -0300

    Estilizando componente de cabeçalho

---
+++ src/components/Header/styles.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+    display: flex;
+    text-align: center;
+    align-items: center;
+    width: 100%;
+    height: 20%;
+    margin-top: 2%;
+    justify-content: space-between;
+`;
+
+export const ContainerLogo = styled.div`
+    width: 14%;
+    height: 8%;
+    margin-left: 8%;
+`;
+
+export const ContainerIndices = styled.div`
+    display: flex;
+    align-items: center;
+    width: 22%;
+    height: 20%;
+    justify-content: space-between;
+`;
+
+export const Indices = styled.h3`
+    font-size: 18pt;
+    color: #6D747A;
+    margin-left: 1%;
+    align-items: center;
+`;
+
+export const ContainerMedia = styled.div`
+    display: flex;
+    align-items: center;
+    width: 30%;
+    height: 20%;
+    margin-top: 2%;
+    justify-content: space-between;
+`;
+
