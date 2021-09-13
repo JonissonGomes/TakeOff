3ade2c8c9347d782d809d2edfd2d1a39a7f3c2d7
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-09-12 23:21:30 -0300

    Criando página de portifólio

--- src/pages/Portifolio/portifolio.jsx
+++ src/pages/Portifolio/portifolio.jsx
@@ -1,15 +1,92 @@
 import React from 'react';
 
-import { Page } from './styles.jsx';
+import { Page, ContainerTitle, Title, Description, ContainerTumb, Tumb, ContainerLocale, City, State, ContainerMedia } from './styles.jsx';
+
+import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header/header';
 import Footer from '../../components/Footer/footer';
+
+import Arraia from '../../Assets/arraia.png';
+import CaboA from '../../Assets/caboA.png';
+import Farol from '../../Assets/farol.png';
+
 
 export default function portifolio() {
     return (
         <Page>
             <Header />
-            
+
+            <ContainerTitle>
+                <Title>{`DISCOVERY A NEW WORLD`}</Title>
+                <Description>{`A melhor experiência passada através das lentes.`}</Description>
+            </ContainerTitle>
+
+            {/* Primeira Linha */}
+            <ContainerTumb>
+                <Tumb>
+                    <Link style={{ textDecoration: 'none' }} to={"/"}>
+                        <img src={Arraia} />
+                        <ContainerLocale>
+                            <City>{"CABO"}</City>
+                            <State>{"PERNAMBUCO"}</State>
+                        </ContainerLocale>
+                    </Link>
+
+                    <ContainerMedia>
+                        <Link style={{ paddingLeft: '2px' }} to="/">
+                            <Icon icon="entypo-social:instagram" fontSize={30} color="#6D747A" />
+                        </Link>
+
+                        <Link to="/">
+                            <Icon icon="ant-design:youtube-filled" fontSize={44} color="#6D747A" />
+                        </Link>
+                    </ContainerMedia>
+                </Tumb>
+
+                <Tumb>
+                    <Link style={{ textDecoration: 'none' }} to={"/"}>
+                        <img src={CaboA} />
+                        <ContainerLocale>
+                            <City>{"ARRAIÁ DO CABO"}</City>
+                            <State>{"PERNAMBUCO"}</State>
+                        </ContainerLocale>
+                    </Link>
+
+                    <ContainerMedia>
+                        <Link style={{ paddingLeft: '2px' }} to="/">
+                            <Icon icon="entypo-social:instagram" fontSize={30} color="#6D747A" />
+                        </Link>
+
+                        <Link to="/">
+                            <Icon icon="ant-design:youtube-filled" fontSize={44} color="#6D747A" />
+                        </Link>
+                    </ContainerMedia>
+                </Tumb>
+
+                <Tumb>
+                    <Link style={{ textDecoration: 'none' }} to={"/"}>
+                        <img src={Farol} />
+                        <ContainerLocale>
+                            <City>{"FAROL DE SALVADOR"}</City>
+                            <State>{"BAHIA"}</State>
+                        </ContainerLocale>
+                    </Link>
+
+                    <ContainerMedia>
+                        <Link style={{ paddingLeft: '2px' }} to="/">
+                            <Icon icon="entypo-social:instagram" fontSize={30} color="#6D747A" />
+                        </Link>
+
+                        <Link to="/">
+                            <Icon icon="ant-design:youtube-filled" fontSize={44} color="#6D747A" />
+                        </Link>
+                    </ContainerMedia>
+                </Tumb>
+            </ContainerTumb>
+
+            <Footer />
         </Page>
     )
 }
--- src/pages/Portifolio/styles.jsx
+++ src/pages/Portifolio/styles.jsx
@@ -4,3 +4,79 @@ export const Page = styled.div`
     width: 100vw;
     height: 40vh;
 `;
+
+export const ContainerTitle = styled.div`
+    margin-top: 6%;
+    text-align: center;
+`;
+
+export const Title = styled.h1`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap');
+    font-family: 'Montserrat', sans;
+    letter-spacing: -0.5px;
+    color: #000000;
+    margin-bottom: 0;
+    font-weight: 900;
+    font-size: 40pt;
+`;
+
+export const Description = styled.h1`
+    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
+    font-family: lato, sans-serif;
+    width: 68%;
+    margin-left: 15%;
+    margin-right: 15%;
+    font-weight: 400;
+    color: #6D747A;
+    font-size: 16pt;
+`;
+
+export const ContainerTumb = styled.div`
+    width: 84vw;
+    height: 60vh;
+    margin-top: 4%;
+    margin-left: 8%;
+    display: flex;
+`;
+
+export const Tumb = styled.div`
+    margin-left: 2.4%;
+    margin-right: 2.4%;
+    width: 480px;
+    height: 240px;
+`;
+
+export const ContainerLocale = styled.div`
+    text-align: center;
+    margin-top: -160px;
+`;
+
+export const City = styled.h2`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap');
+    font-family: 'Montserrat', sans;
+    color: white;
+    font-weight: 900;
+    font-size: 30pt;
+    letter-spacing: -0.5px;
+    margin-bottom: 0;
+`;
+
+export const State = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap');
+    font-family: 'Montserrat', sans;
+    color: white;
+    font-weight: 400;
+    font-size: 12pt;
+    letter-spacing: -0.5px;
+    margin-top: 0;
+    margin-bottom: 13%;
+`;
+
+export const ContainerMedia = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 18%;
+    margin-left: 40%;
+    margin-right: 40%;
+`;
