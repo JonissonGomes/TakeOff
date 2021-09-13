1b45a571bf0ee30fb13b5d16b720154a4b2e7d1b
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-09-12 21:30:41 -0300

    Adicionando [Portifolio] a rota de navegação

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from './pages/Home/home';
+import Portifolio from './pages/Portifolio/portifolio';
 
 export default function Routes() {
     return (
         <Router>
                 <Switch>
                     <Route exact path="/" component={Home} />
+                    <Route path="/Portifolio" component={Portifolio} />
                 </Switch>
         </Router>
     );
