ee2f714cc3b265beb300e67981881cd0d2663b70
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-09-12 21:31:21 -0300

    Inicializando pagáina [Portifolio]

---
+++ src/pages/Portifolio/portifolio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Page } from './styles.jsx';
+
+import Header from '../../components/Header/header';
+import Footer from '../../components/Footer/footer';
+
+export default function portifolio() {
+    return (
+        <Page>
+            <Header />
+            
+        </Page>
+    )
+}
