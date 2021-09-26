19bc71449cefcd6c1e9fb1e2b754d1590d110b77
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-09-26 00:19:14 -0300

    Estilizando modal e refatorando alguns elementos

--- src/pages/Galeria/styles.jsx
+++ src/pages/Galeria/styles.jsx
@@ -11,7 +11,6 @@ export const ContainerTitle = styled.div`
 `;
 
 export const Title = styled.h1`
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap');
     font-family: 'Montserrat', sans;
     letter-spacing: -0.5px;
     color: #000000;
@@ -21,7 +20,6 @@ export const Title = styled.h1`
 `;
 
 export const Description = styled.h1`
-    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
     font-family: lato, sans-serif;
     width: 68%;
     margin-left: 15%;
@@ -34,13 +32,12 @@ export const Description = styled.h1`
 export const ContainerFilter = styled.div`
     display: flex;
     align-items: center;
-    margin-left: 14%;
+    margin-left: 11%;
     margin-right: 16%;
     margin-top: 2%;
 `;
 
 export const FilterLabel = styled.h1`
-    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
     font-family: lato, sans-serif;
     letter-spacing: 0;
     color: #6D747A;
@@ -84,18 +81,25 @@ export const Clear = styled.div`
 `;
 
 export const ContainerImages = styled.div`
-    display: flex;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr ;
+    row-gap: 4%;
+    column-gap: 2%;
     margin-top: 2%;
-    margin-left: 10%;
+    margin-left: 12%;
+    width: 50%;
+    /* @media(max-width: 700px){
+        grid-template-columns: 1fr 1fr;
+    } */
 `;
 
 export const ContainerInfo = styled.div`
     border-radius: 4px;
     position: absolute;
     width: 380px;
-    height: 158px;
+    height: 194px;
     opacity: 0;
-    margin-top: -8%;
+    margin-top: -196px;
     transition: 400ms;
 `;
 
@@ -113,43 +117,56 @@ export const Content = styled.div`
       }
 `;
 
+export const SectionButton = styled.div`
+    margin-top: 1%;
+    margin-left: 96%;
+    margin-bottom: 0;
+    width: 30px;
+`;
+
 export const Image = styled.img`
     width: 380px;
+    height: 194px;
 `;
 
 export const Cidade = styled.p`
-    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
     font-family: lato, sans-serif;
     letter-spacing: 0;
     color: white;
-    margin-top: 6%;
+    margin-top: 10%;
     margin-bottom: 0;
     margin-left: 2%;
     font-weight: 900;
-    font-size: 18pt;
+    font-size: 22pt;
 `;
 
 export const Estado = styled.p`
-    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
     font-family: lato, sans-serif;
     letter-spacing: 1px;
     color: white;
     margin-top: -1%;
     margin-bottom: 0;
     margin-left: 2%;
-    font-weight: 100;
-    font-size: 10pt;
+    font-weight: 300;
+    font-size: 14pt;
 `;
 
 export const More = styled.p`
-    border: 1px solid white;
     border-radius: 25px;
-    color: white;
+    color: black;
+    border: 1px solid black;
+    background-color: white;
     margin-left: 36%;
     margin-bottom: 0;
     margin-right: 36%;
     padding: 4px;
     text-align: center;
+    &:hover{
+        color: white;
+        background-color: #00000038;
+        border: 1px solid white;
+        cursor: pointer;
+    }
 `;
 
 export const Redes = styled.p`
@@ -159,4 +176,83 @@ export const Redes = styled.p`
     margin-left: 40%;
     justify-content: center;
     width: 20%;
+`;
+
+export const Modal = styled.div`
+    width: 100%;
+    height: 106.9%;
+    position: absolute;
+    margin-top: -46%;
+    margin-bottom: 0;
+    background-color: #0009;
+    opacity: 0.8;
+`;
+export const ContainerModal = styled.div`
+    background-color: blue;
+    display: flex;
+`;
+
+export const ContentModal = styled.div`
+    background-color: white;
+    width: 1448px;
+    height: 860px;
+    margin-left: 14%;
+    margin-top: -36%;
+    margin-bottom: 0;
+    border-radius: 4px;
+    border: 1px solid black;
+    position: absolute;
+`;
+
+export const ImageModal = styled.img`
+    width: 800px;
+    height: 600px;
+    margin-left: 2%;
+    margin-top: 2%;
+`;
+
+export const Info = styled.div`
+    background-color: blue;
+`;
+
+export const Locale = styled.h1`
+ font-family: lato, sans-serif;
+    letter-spacing: 0;
+    color: white;
+    margin-top: 10%;
+    margin-bottom: 0;
+    margin-left: 2%;
+    font-weight: 900;
+    font-size: 22pt;
+`;
+
+export const SubLocale = styled.h3`
+ font-family: lato, sans-serif;
+    letter-spacing: 0;
+    color: white;
+    margin-top: 10%;
+    margin-bottom: 0;
+    margin-left: 2%;
+    font-weight: 900;
+    font-size: 22pt;
+`;
+
+export const Details = styled.p`
+ font-family: lato, sans-serif;
+    letter-spacing: 0;
+    color: white;
+    margin-top: 10%;
+    margin-bottom: 0;
+    margin-left: 2%;
+    font-weight: 900;
+    font-size: 22pt;
+`;
+
+export const Data = styled.p`
+    font-weight: 900;
+    font-size: 22pt;
+`;
+
+export const MediaLink = styled.div`
+    background-color: blue;
 `;
